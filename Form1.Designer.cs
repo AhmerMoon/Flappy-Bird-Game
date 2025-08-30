3aedca127c70f13cfd666fa679fb3cc30a4956f3
Ahmer Moon <ahmermoonmajid@gmail.com>
2025-08-30 07:38:57 +0500

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Flappy_Bird_Windows_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.scoreText = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.menuPanel = new System.Windows.Forms.Panel();
+            this.lblQuit = new System.Windows.Forms.Label();
+            this.lblCredits = new System.Windows.Forms.Label();
+            this.lblStartGame = new System.Windows.Forms.Label();
+            this.pipeTop = new System.Windows.Forms.PictureBox();
+            this.cloud = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.pipeBottom = new System.Windows.Forms.PictureBox();
+            this.clouds3 = new System.Windows.Forms.PictureBox();
+            this.flappyBird = new System.Windows.Forms.PictureBox();
+            this.menuPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cloud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clouds3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.BackColor = System.Drawing.Color.Moccasin;
+            this.scoreText.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreText.Location = new System.Drawing.Point(1204, 707);
+            this.scoreText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(149, 46);
+            this.scoreText.TabIndex = 4;
+            this.scoreText.Text = "Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // menuPanel
+            // 
+            this.menuPanel.Controls.Add(this.lblQuit);
+            this.menuPanel.Controls.Add(this.lblCredits);
+            this.menuPanel.Controls.Add(this.lblStartGame);
+            this.menuPanel.Location = new System.Drawing.Point(739, 329);
+            this.menuPanel.Name = "menuPanel";
+            this.menuPanel.Size = new System.Drawing.Size(333, 349);
+            this.menuPanel.TabIndex = 5;
+            // 
+            // lblQuit
+            // 
+            this.lblQuit.AutoSize = true;
+            this.lblQuit.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblQuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.lblQuit.Location = new System.Drawing.Point(31, 261);
+            this.lblQuit.Name = "lblQuit";
+            this.lblQuit.Size = new System.Drawing.Size(119, 60);
+            this.lblQuit.TabIndex = 2;
+            this.lblQuit.Text = "Quit";
+            this.lblQuit.Click += new System.EventHandler(this.lblQuit_Click);
+            // 
+            // lblCredits
+            // 
+            this.lblCredits.AutoSize = true;
+            this.lblCredits.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCredits.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.lblCredits.Location = new System.Drawing.Point(31, 136);
+            this.lblCredits.Name = "lblCredits";
+            this.lblCredits.Size = new System.Drawing.Size(186, 60);
+            this.lblCredits.TabIndex = 1;
+            this.lblCredits.Text = "Credits";
+            this.lblCredits.Click += new System.EventHandler(this.lblCredits_Click);
+            // 
+            // lblStartGame
+            // 
+            this.lblStartGame.AutoSize = true;
+            this.lblStartGame.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblStartGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.lblStartGame.Location = new System.Drawing.Point(31, 12);
+            this.lblStartGame.Name = "lblStartGame";
+            this.lblStartGame.Size = new System.Drawing.Size(283, 60);
+            this.lblStartGame.TabIndex = 0;
+            this.lblStartGame.Text = "Start Game";
+            this.lblStartGame.Click += new System.EventHandler(this.lblStartGame_Click);
+            // 
+            // pipeTop
+            // 
+            this.pipeTop.Image = global::Flappy_Bird_Windows_Form.Properties.Resources.pipedown;
+            this.pipeTop.Location = new System.Drawing.Point(638, -217);
+            this.pipeTop.Margin = new System.Windows.Forms.Padding(4);
+            this.pipeTop.Name = "pipeTop";
+            this.pipeTop.Size = new System.Drawing.Size(133, 438);
+            this.pipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop.TabIndex = 0;
+            this.pipeTop.TabStop = false;
+            // 
+            // cloud
+            // 
+            this.cloud.Image = global::Flappy_Bird_Windows_Form.Properties.Resources.Cloud;
+            this.cloud.Location = new System.Drawing.Point(847, 119);
+            this.cloud.Name = "cloud";
+            this.cloud.Size = new System.Drawing.Size(149, 89);
+            this.cloud.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cloud.TabIndex = 6;
+            this.cloud.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Image = global::Flappy_Bird_Windows_Form.Properties.Resources.ground;
+            this.ground.Location = new System.Drawing.Point(-21, 779);
+            this.ground.Margin = new System.Windows.Forms.Padding(4);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(2255, 387);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 3;
+            this.ground.TabStop = false;
+            // 
+            // pipeBottom
+            // 
+            this.pipeBottom.Image = global::Flappy_Bird_Windows_Form.Properties.Resources.pipe;
+            this.pipeBottom.Location = new System.Drawing.Point(475, 489);
+            this.pipeBottom.Margin = new System.Windows.Forms.Padding(4);
+            this.pipeBottom.Name = "pipeBottom";
+            this.pipeBottom.Size = new System.Drawing.Size(145, 567);
+            this.pipeBottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottom.TabIndex = 1;
+            this.pipeBottom.TabStop = false;
+            // 
+            // clouds3
+            // 
+            this.clouds3.Image = global::Flappy_Bird_Windows_Form.Properties.Resources.Clouds2;
+            this.clouds3.Location = new System.Drawing.Point(198, 36);
+            this.clouds3.Name = "clouds3";
+            this.clouds3.Size = new System.Drawing.Size(271, 89);
+            this.clouds3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.clouds3.TabIndex = 7;
+            this.clouds3.TabStop = false;
+            // 
+            // flappyBird
+            // 
+            this.flappyBird.Image = ((System.Drawing.Image)(resources.GetObject("flappyBird.Image")));
+            this.flappyBird.Location = new System.Drawing.Point(79, 243);
+            this.flappyBird.Margin = new System.Windows.Forms.Padding(4);
+            this.flappyBird.Name = "flappyBird";
+            this.flappyBird.Size = new System.Drawing.Size(115, 93);
+            this.flappyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.flappyBird.TabIndex = 8;
+            this.flappyBird.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.flappyBird);
+            this.Controls.Add(this.clouds3);
+            this.Controls.Add(this.cloud);
+            this.Controls.Add(this.menuPanel);
+            this.Controls.Add(this.pipeTop);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pipeBottom);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Malik Flappy Bird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyisup);
+            this.menuPanel.ResumeLayout(false);
+            this.menuPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cloud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clouds3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pipeTop;
+        private System.Windows.Forms.PictureBox pipeBottom;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Panel menuPanel;
+        private System.Windows.Forms.Label lblQuit;
+        private System.Windows.Forms.Label lblCredits;
+        private System.Windows.Forms.Label lblStartGame;
+        private System.Windows.Forms.PictureBox cloud;
+        private System.Windows.Forms.PictureBox clouds3;
+        private System.Windows.Forms.PictureBox flappyBird;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrayNotify;
+
+namespace Flappy_Bird_Windows_Form
+{
+    public partial class Form1 : Form
+    {
+        int basePipeSpeed = 12; // Base pipe speed
+        int baseCloudSpeed = 8; // Base cloud speed
+        int gravity = 10; // Default gravity
+        int score = 0; // Default score
+        int speedIncrement = 1; // Speed increment factor
+        int maxPipeSpeed = 20; // Maximum pipe speed
+        int maxCloudSpeed = 20; // Maximum cloud speed
+
+        SoundPlayer backgroundPlayer = new SoundPlayer("D:/MarioPhonk.wav");
+        SoundPlayer gameOverPlayer = new SoundPlayer("D:/GameOver.wav");
+
+        Label lblGameOver; // Label for Game Over message
+        Label lblCountdown; // Label for countdown timer
+        Timer delayTimer; // Timer for delay before showing menu
+        Timer countdownTimer; // Timer for countdown
+        int countdownValue; // Countdown value
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeGameOverLabel();
+            InitializeCountdownLabel(); // Initialize the countdown label
+            InitializeDelayTimer(); // Initialize the delay timer
+            InitializeCountdownTimer(); // Initialize the countdown timer
+            ShowMenu(); // Show menu on form load
+        }
+
+        private void InitializeGameOverLabel()
+        {
+            // Initialize the Game Over label
+            lblGameOver = new Label
+            {
+                Text = "", // Initially empty
+                Font = new Font("Arial", 30, FontStyle.Bold),
+                ForeColor = Color.Red,
+                Location = new Point(300, 200),
+                AutoSize = true,
+                Visible = false // Hidden initially
+            };
+            this.Controls.Add(lblGameOver);
+        }
+
+        private void InitializeCountdownLabel()
+        {
+            // Initialize the Countdown label
+            lblCountdown = new Label
+            {
+                Text = "", // Initially empty
+                Font = new Font("Arial", 50, FontStyle.Bold),
+                ForeColor = Color.Black,
+                Location = new Point(350, 200),
+                AutoSize = true,
+                Visible = false // Hidden initially
+            };
+            this.Controls.Add(lblCountdown);
+        }
+
+        private void InitializeDelayTimer()
+        {
+            // Initialize the delay timer
+            delayTimer = new Timer();
+            delayTimer.Interval = 3000; // Set delay to 3 seconds
+            delayTimer.Tick += DelayTimer_Tick; // Subscribe to the Tick event
+        }
+
+        private void InitializeCountdownTimer()
+        {
+            // Initialize the countdown timer
+            countdownTimer = new Timer();
+            countdownTimer.Interval = 1000; // Set interval to 1 second
+            countdownTimer.Tick += CountdownTimer_Tick; // Subscribe to the Tick event
+        }
+
+        private void ShowMenu()
+        {
+            // Show the menu panel and hide score
+            menuPanel.Visible = true;
+            scoreText.Visible = false;
+            lblGameOver.Visible = false;
+            lblCountdown.Visible = false; // Hide countdown label
+
+            // Show stationary bird and pipes
+            flappyBird.Visible = true;
+            pipeTop.Visible = true;
+            pipeBottom.Visible = true;
+            ground.Visible = true;
+
+            // Stop background music and ensure game objects don't move
+            backgroundPlayer.Stop();
+            gameTimer.Stop();
+        }
+
+        private void StartGame()
+        {
+            // Hide the menu panel and enable game elements
+            menuPanel.Visible = false;
+            scoreText.Visible = true;
+
+            lblGameOver.Visible = false; // Hide the Game Over label
+
+            // Reset game variables
+            score = 0; // Reset score
+            scoreText.Text = "Score: " + score; // Reset score display
+
+            // Reset speeds to base values
+            basePipeSpeed = 8; // Reset pipe speed to base
+            baseCloudSpeed = 8; // Reset cloud speed to base
+
+            // Reset positions of game objects
+            flappyBird.Top = 200; // Reset bird position
+            pipeTop.Left = 900; // Reset top pipe position
+            pipeBottom.Left = 500; // Reset bottom pipe position
+
+            // Start the countdown
+            countdownValue = 3; // Set countdown to 3 seconds
+            lblCountdown.Text = countdownValue.ToString(); // Display countdown
+            lblCountdown.Visible = true; // Show countdown label
+            countdownTimer.Start(); // Start the countdown timer
+        }
+
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            countdownValue--; // Decrease countdown value
+            lblCountdown.Text = countdownValue.ToString(); // Update countdown display
+
+            if (countdownValue <= 0)
+            {
+                countdownTimer.Stop(); // Stop the countdown timer
+                lblCountdown.Visible = false; // Hide countdown label
+
+                // Start the background music and game timer
+                backgroundPlayer.PlayLooping();
+                gameTimer.Start();
+            }
+        }
+
+        private void endGame()
+        {
+            // Stop the game timer and background music
+            gameTimer.Stop();
+            backgroundPlayer.Stop();
+
+            // Play the game-over sound
+            gameOverPlayer.Play();
+
+            // Display the Game Over label
+            lblGameOver.Text = "Game Over! Score: " + score;
+            lblGameOver.Visible = true;
+
+            // Start the delay timer
+            delayTimer.Start();
+        }
+
+        private void DelayTimer_Tick(object sender, EventArgs e)
+        {
+            // Stop the delay timer
+            delayTimer.Stop();
+
+            // Show the menu again
+            ShowMenu();
+        }
+
+        private void QuitGame()
+        {
+            // Exit the application
+            Application.Exit();
+        }
+
+        private void ShowCredits()
+        {
+            // Show credits message box
+            MessageBox.Show("Flappy Bird Game\nDeveloped by Malik\n© 2024", "Credits");
+        }
+
+        Random random = new Random();
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            // Allow game components to move only after the game starts
+            flappyBird.Top += gravity; // Apply gravity to the bird
+            pipeBottom.Left -= basePipeSpeed; // Move the bottom pipe
+            pipeTop.Left -= basePipeSpeed; // Move the top pipe
+            cloud.Left -= baseCloudSpeed; // Move the cloud
+            clouds3.Left -= baseCloudSpeed; // Move the third cloud
+            scoreText.Text = "Score: " + score; // Update the score text
+
+            // Reset pipe positions and increment score
+            if (pipeBottom.Left < -100)
+            {
+                pipeBottom.Left = random.Next(1800, 2200);
+                pipeBottom.Top = random.Next(300, 500); // Randomize bottom pipe position
+                score++;
+            }
+            if (pipeTop.Left < -100)
+            {
+                pipeTop.Left = random.Next(1800, 2200);
+                pipeTop.Top = pipeBottom.Top - pipeBottom.Height - 150; // Maintain a gap
+                score++;
+            }
+            if (cloud.Left < -350)
+            {
+                cloud.Left = random.Next(1200, 1600); // Randomize cloud position
+                cloud.Top = random.Next(50, 300);    // Randomize cloud height
+            }
+
+            if (clouds3.Left < -350)
+            {
+                clouds3.Left = random.Next(1200, 1600); // Randomize clouds3 position
+                clouds3.Top = random.Next(50, 300);    // Randomize clouds3 height
+            }
+
+            // Check for collisions or out-of-bounds
+            if (flappyBird.Bounds.IntersectsWith(pipeBottom.Bounds) ||
+                flappyBird.Bounds.IntersectsWith(pipeTop.Bounds) ||
+                flappyBird.Bounds.IntersectsWith(ground.Bounds) ||
+                flappyBird.Top < -25)
+            {
+                endGame();
+            }
+
+            // Increase speed based on score but cap it at max speed
+            if (score > 0 && score % 10 == 0)
+            {
+                if (basePipeSpeed < maxPipeSpeed)
+                {
+                    basePipeSpeed += speedIncrement; // Increase pipe speed
+                    baseCloudSpeed += speedIncrement; // Increase cloud speed
+                }
+            }
+        }
+
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+            // If the space key is pressed, set gravity to -10 (jump)
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -10;
+            }
+        }
+
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+            // If the space key is released, set gravity back to 10 (fall)
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 10;
+            }
+        }
+
+        // Event handlers for menu labels
+        private void lblStartGame_Click(object sender, EventArgs e)
+        {
+            StartGame(); // Start the game
+        }
+
+        private void lblCredits_Click(object sender, EventArgs e)
+        {
+            ShowCredits(); // Show credits
+        }
+
+        private void lblQuit_Click(object sender, EventArgs e)
+        {
+            QuitGame(); // Quit the application
+        }
+    }
+}
